f49697a6016d0049156e333a8ad7d0a61a5e3766
Brahmamdevops <brahmamm32@gmail.com>
2023-12-17 19:31:13 +0530

    bug fix

--- 08-installpackage.sh
+++ 08-installpackage.sh
@@ -6,8 +6,11 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
     then 
         echo : "please install with root user"
+        exit 1
     else
         echo : "i am root user installing package"
 fi
 
 yum install mysql -y
+
+
