41d8944b4633e80eaa596b53d3dce2bd424e9b45
Brahmamdevops <brahmamm32@gmail.com>
2023-12-17 13:21:24 +0530

    array practicing

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 
-names=("ram" "laxman" "sita")
+names=("ram" "laxman" "sita" "hanuman") 
 
 echo :" first index of array is ${names[0]}"
+
+echo :" first second of array is ${names[1]}"
+
+
+echo :" array is ${names[@]}"
