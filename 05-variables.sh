1a7420b949f96a60d8af42ad98342effc7a3b5d3
Brahmamdevops <brahmamm32@gmail.com>
2023-12-17 13:03:58 +0530

    practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,15 @@
 
 echo : "enter your user name"
 
+read  username
+
+echo : "enter your password"
+
+read  password
+
+echo : "this is my $username, and $password "
+
+
 read -s username
 
 echo : "enter your password"
