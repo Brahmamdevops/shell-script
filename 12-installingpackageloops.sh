c50a635f779fb72e1029ab6c5dd1c36dca42ddf9
Brahmamdevops <brahmamm32@gmail.com>
2023-12-18 12:15:49 +0530

    installing packages using loops

--- 12-installingpackageloops.sh
+++ 12-installingpackageloops.sh
@@ -6,10 +6,10 @@ echo " $0 is started"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-R='\e[31m'
-G='\e[32m'
-Y='\e[33m'
-N='\e[0m'
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
 
 validate(){
     if [ $1 -ne 0 ]
